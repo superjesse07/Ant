c73b972efdec837c80e524e787e23bb339e0bcfc
Jesse Rosendahl <jesse@jessar.nl>
2023-02-22 10:58:50 +0100

    setup for code

---
+++ Assets/Scripts/CellData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public struct CellData
+{
+    public float foodPheromone;
+    public float nestPheromone;
+    public float deathPheromone;
+
+    public CellData(float foodPheromone, float nestPheromone, float deathPheromone)
+    {
+        this.foodPheromone = foodPheromone;
+        this.nestPheromone = nestPheromone;
+        this.deathPheromone = deathPheromone;
+    }
+
+    public Color ToColor(Color food, Color nest, Color death)
+    {
+        return (food * foodPheromone + nest * nestPheromone + death * deathPheromone) /3f;
+    }
+}
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public static Settings Instance { private set; get; }
+
+    [Header("Pheromone colors")] public Color foodPheromone, nestPheromone, deathPheromone;
+
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+}
